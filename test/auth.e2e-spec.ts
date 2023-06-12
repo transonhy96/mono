171439d1fad40f61a9c2add2aa662c28c37fa709
Sontran <transonhy96@gmail.com>
2023-06-12 16:33:21 +0700

    test(shares): SharesController integration test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
+import { PrismaService } from "src/prisma/prisma.service";
 
 describe("AuthController (e2e)", () => {
   let app: INestApplication;
@@ -9,10 +10,14 @@ describe("AuthController (e2e)", () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [PrismaService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    const prisma = moduleFixture.get<PrismaService>(PrismaService);
+    await app.init();
+    await prisma.user.deleteMany({});
   });
 
   it(route + "signup " + "should create new user", () => {
